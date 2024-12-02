674d55324362c86ac669ab10cac4407dd7e0f400
gihan444 <chamikabinance7@gmail.com>
2024-12-02 12:44:02 +0530

    Update session.js

--- session.js
+++ session.js
@@ -21,5 +21,5 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "your session_id here",
+SESSION_ID: process.env.SESSION_ID || "ANJU-MD=GR0RULTC#0gZnRcB2hafVa1DSpRSNcHPC8uGIm7YIeASC3BSXltE",
 };
